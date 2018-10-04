303e6c6b83f983b84a3c293c377f310ee77358b0
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-04 16:49:45 -0400

    Added changes with the help of good buddy Luke P

---
+++ app.js
@@ -0,0 +1,9 @@
+var friends = ['Matt', 'Jacob','Ann', 'Kimberly', 'David', 'Kyle', 'Brian']
+for(var i = 0; i<friends.length; i++)   {
+    console.log(friends[i]);
+
+if (friends [i] == 'David') {
+    console.log ('Found Him!');
+    break;
+    }
+}
