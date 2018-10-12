4879def959bf67133bd170121b45c8a5c7b1cc7c
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-12 19:24:17 -0400

    added new variable and if else statement to main game function in order to toggle between X and O

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-
 let isPlayer1 = true;
 let isPlayer2 = false;
-
+let numClicked = 1;
 
 let row = document.querySelectorAll('.row > div');
 // row.addEventListener('click', function(e) { 
 //     e.target.innerHTML = ('X');
 // });
 console.log(row);
-row.forEach(function(cell)  {
+row.forEach(function (cell) {
     cell.addEventListener("click", cellClicked);
 });
 // main game logic
 function cellClicked(e) {
-    e.target.textContent = 'HI';
-    togglePlayer();
+    //Assignment operator to increment var by one insteaad of arithmetic operator
+    numClicked += 1;
+    if (numClicked % 2 == 0) {
+        e.target.textContent = 'X';
+
+    } else if (numClicked % 2 != 0) {
+        e.target.textContent = 'O';
+
+    }
+    
+    // togglePlayer();
 }
 // Helper functions
-function togglePlayer ()    {
+function togglePlayer() {
     console.log('togglePlayer');
 }
